ee915676261eb1763f879a69f2bfb24779d3915b
Arthur Lima <arthur-esc@hotmail.com>
2021-07-14 18:50:42 -0300

    Add custom lualine components written in lua 🌕

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,9 +1,36 @@
 local function filepath()
-  return '%f'
+  local dirty = ''
+
+  if vim.bo.modified then
+    dirty = ' *'
+  end
+
+  return '%f' .. dirty
 end
 
 local function breadcrumb()
   return vim.fn['nvim_treesitter#statusline']()
+end
+
+local function lsp_diag()
+  local result = {}
+  local actual_buffer = vim.api.nvim_get_current_buf()
+  local levels = {
+    E = 'Error',
+    W = 'Warning',
+    I = 'Information',
+    H = 'Hint'
+  }
+
+  for key, level in pairs(levels) do
+    print(vim.inspect(levels))
+    local level_count = vim.lsp.diagnostic.get_count(actual_buffer, level)
+    if level_count > 0 then
+      table.insert(result, key .. ': ' .. level_count)
+    end
+  end
+
+  return table.concat(result, ' / ')
 end
 
 require('lualine').setup {
@@ -11,10 +38,23 @@ require('lualine').setup {
     theme = 'nightfly'
   },
   sections = {
-    lualine_a = {'branch'},
+    lualine_a = {
+      'branch',
+      {
+        'diff',
+        colored = true,
+        symbols = {added = '+', modified = '~', removed = '-'}
+      }
+    },
     lualine_b = {filepath, breadcrumb},
     lualine_c = {''},
-    lualine_x = {'filetype'},
+    lualine_x = {
+      {
+        lsp_diag,
+        color = 'WarningMsg'
+      },
+      'filetype'
+    },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },

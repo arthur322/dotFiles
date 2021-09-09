dc76c51dd2a6207ac872974ae190cb0070878087
Arthur Lima <arthur-esc@hotmail.com>
2021-09-09 11:37:54 -0300

    Buffer line update
    This adds new mappings for buffer navigation using
    `<leader>buffer_number`, because the default mappings option is no more
    suported by the plugin.

--- nvim/after/plugin/nvim-bufferline.lua
+++ nvim/after/plugin/nvim-bufferline.lua
@@ -1,12 +1,15 @@
 require'bufferline'.setup({
   options = {
-    numbers = "none",
+    numbers = function(opts)
+      return string.format('%s', opts.ordinal)
+    end, -- :h bufferline-numbers
     separator_style = "thick",
     diagnostics = "nvim_lsp",
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       local icon = level:match("error") and " " or " "
       return " " .. icon .. count
-    end
+    end,
+    sort_by = "directory",
   }
 })
 
@@ -15,4 +18,15 @@ vim.api.nvim_set_keymap('n', '<leader><leader><Tab>', "<cmd>BufferLinePick<CR>",
 vim.api.nvim_set_keymap('n', '<leader>B', "<cmd>BufferLinePickClose<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Tab>', "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-Tab>', "<cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><S-Tab>', "<cmd>BufferLineCyclePrev<CR>:bdelete #<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>1', "<cmd>BufferLineGoToBuffer 1<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>2', "<cmd>BufferLineGoToBuffer 2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>3', "<cmd>BufferLineGoToBuffer 3<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>4', "<cmd>BufferLineGoToBuffer 4<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>5', "<cmd>BufferLineGoToBuffer 5<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>6', "<cmd>BufferLineGoToBuffer 6<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>7', "<cmd>BufferLineGoToBuffer 7<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>8', "<cmd>BufferLineGoToBuffer 8<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>9', "<cmd>BufferLineGoToBuffer 9<CR>", { noremap = true, silent = true })
 

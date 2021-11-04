eac75eba1d7e387f3c501f946c4614542ccb99c4
Arthur Lima <arthur-esc@hotmail.com>
2021-11-04 10:04:28 -0300

    Rounded lualine setup

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -30,7 +30,10 @@ end
 
 require('lualine').setup {
   options = {
-    theme = 'solarized_light'
+    icons_enabled = true,
+    theme = 'papercolor_light',
+    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''}
   },
   sections = {
     lualine_a = {

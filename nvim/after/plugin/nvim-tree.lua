36799b74f23fc18dd4cdaf494b7d59ae7a22fa33
Ana Reis <analuizareism@hotmail.com>
2022-01-05 21:55:18 -0300

    Better nvim-tree configurations to work with vim-vinegar

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,8 +1,8 @@
-vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_indent_markers = 0
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 3
 vim.g.nvim_tree_icon_padding = ' '
-vim.g.nvim_tree_refresh_wait = 500
+vim.g.nvim_tree_refresh_wait = 200
 
 vim.cmd([[
   highlight NvimTreeFolderIcon guifg=DarkBlue
@@ -11,10 +11,14 @@ vim.cmd([[
 require'nvim-tree'.setup {
   disable_netrw = false,
   hijack_netrw = false,
-  open_on_setup = true,
+  open_on_setup = false,
+  update_to_buf_dir   = {
+    enable = false,
+    auto_open = false,
+  },
   update_focused_file = {
     enable = true,
-    update_cwd = false,
+    update_cwd = true,
     ignore_list = {'node_modules'},
   },
   diagnostics = {
@@ -24,14 +28,14 @@ require'nvim-tree'.setup {
     width = 40,
     side = 'left',
     auto_resize = true,
-    relativenumber = true,
+    relativenumber = false,
   },
 }
 
 vim.api.nvim_set_keymap(
   'n',
   '<leader>e',
-  "<cmd>NvimTreeFindFile<CR>",
+  "<cmd>NvimTreeFindFileToggle<CR>",
   { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap(

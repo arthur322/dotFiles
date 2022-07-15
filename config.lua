7a2d94202faf614bc1c5be6fec9b54a6b83e6429
Arthur Lima <arthur-esc@hotmail.com>
2022-07-15 18:15:53 -0300

    Updated lvim config file

--- config.lua
+++ config.lua
@@ -10,69 +10,57 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = false
-vim.o.background = "dark"
-lvim.colorscheme = "onedarkpro"
-vim.wo.relativenumber = true;
-vim.o.cmdheight = 1
+-- lvim.colorscheme = "onedarker"
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = false
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
-lvim.keys.normal_mode["<C-x>"] = ":q<cr>"
--- Scroll with <C-j> and <C-k>
-lvim.keys.normal_mode["<C-j>"] = "2<C-e>"
-lvim.keys.normal_mode["<C-k>"] = "2<C-y>"
--- Remove ctrlz's suspend
-lvim.keys.normal_mode["<C-z>"] = "<Esc>"
+-- vim.keymap.del("n", "<C-Up>")
+-- override a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
-local _, actions = pcall(require, "telescope.actions")
-lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-  -- for normal mode
-  n = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-}
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
+--   -- for input mode
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
+--   -- for normal mode
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["<tab>"] = { "<C-^>", "Last Buffer" }
 -- lvim.builtin.which_key.mappings["t"] = {
 --   name = "+Trouble",
 --   r = { "<cmd>Trouble lsp_references<cr>", "References" },
 --   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
 --   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
 --   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 -- }
--- which-key timeout
-vim.o.timeoutlen = 500
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -98,13 +86,17 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- ---@usage disable automatic installation of servers
 -- lvim.lsp.automatic_servers_installation = false
 
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -151,14 +143,90 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- }
 
 -- Additional Plugins
+-- lvim.plugins = {
+--     {"folke/tokyonight.nvim"},
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
+
+
+-- #################### Custom Configs ##################
+-- General
+vim.o.scrolloff = 5
+vim.o.list = true
+vim.opt.listchars = 'eol:¬,tab:>·,trail:.,extends:>,precedes:<,multispace:· '
+vim.opt.relativenumber = true;
+vim.o.cmdheight = 1
+lvim.format_on_save = false
+
+-- Colors
+vim.o.background = "light"
+vim.g.material_style = "lighter" -- darker, lighter, oceanic, palenight, deep ocean
+vim.g.ayucolor = "light" -- light, mirage, dark
+vim.g.catppuccin_flavour = "mocha" -- latte, mocha, frappe, macchiato
+lvim.colorscheme = "catppuccin"
+
+-- Additional Plugins
 lvim.plugins = {
-  {"sainnhe/sonokai"},
-  {"tpope/vim-surround"},
-  {"Xuyuanp/scrollbar.nvim"},
-  {"olimorris/onedarkpro.nvim"},
-  {"editorconfig/editorconfig-vim"},
-  {"lunarvim/colorschemes"},
+  { "sainnhe/sonokai" },
+  { "tpope/vim-surround" },
+  { "Xuyuanp/scrollbar.nvim" },
+  { "olimorris/onedarkpro.nvim" },
+  { "editorconfig/editorconfig-vim" },
+  { "lunarvim/colorschemes" },
+  { "Th3Whit3Wolf/one-nvim" },
+  { "ThePrimeagen/harpoon" },
+  { "tpope/vim-fugitive" },
+  { "marko-cerovac/material.nvim" },
+  { "tpope/vim-repeat" },
+  { "catppuccin/nvim", as = "catppuccin" },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   requires = {
+  --     { "nvim-telescope/telescope-live-grep-args.nvim" },
+  --   },
+  --   config = function()
+  --     require("telescope").load_extension("live_grep_args")
+  --   end
+  -- },
+  { "ayu-theme/ayu-vim" },
+  {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end
+  }
 }
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+-- save and close buffers
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<C-x>"] = ":q<cr>"
+-- Scroll with <C-j> and <C-k>
+lvim.keys.normal_mode["<C-j>"] = "2<C-e>"
+lvim.keys.normal_mode["<C-k>"] = "2<C-y>"
+-- Remove ctrlz's suspend
+lvim.keys.normal_mode["<C-z>"] = "<Esc>"
+
+-- Which key "alt tab" buffers
+lvim.builtin.which_key.mappings["<tab>"] = { "<C-^>", "Last Buffer" }
+lvim.builtin.which_key.mappings["<tab>"] = { "<C-^>", "Last Buffer" }
 
 -- scrollbar.nvim plugin setup
 vim.cmd([[
@@ -176,23 +244,132 @@ let g:scrollbar_shape = {
   \ }
 ]])
 
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- lvim.autocommands.custom_groups = {
---   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
--- }
-
 -- better folder config
 vim.opt.foldmethod = "expr" -- folding set to "expr" for treesitter based folding
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
 vim.opt.foldlevelstart = 20
 
 -- Linters
+---@diagnostic disable-next-line: redundant-parameter
 require("null-ls").setup({
-    sources = {
-        require("null-ls").builtins.formatting.prettier,
-        require("null-ls").builtins.formatting.eslint,
-        -- require("null-ls").builtins.diagnostics.eslint_d,
-        require("null-ls").builtins.diagnostics.eslint,
-    },
+  sources = {
+    require("null-ls").builtins.formatting.prettierd,
+    -- require("null-ls").builtins.formatting.eslint,
+    require("null-ls").builtins.code_actions.eslint_d,
+    require("null-ls").builtins.diagnostics.eslint_d,
+  },
+})
+vim.lsp.buf.format({ timeout_ms = 2000 })
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+lvim.autocommands.custom_groups = {
+  { "BufEnter", "NvimTree", "set cursorline" },
+} -- Cursor line highlight on focused buffer
+
+-- Harpoon
+lvim.builtin.which_key.mappings["["] = { ':lua require("harpoon.ui").toggle_quick_menu()<CR>', "Open Harpoon menu" }
+lvim.builtin.which_key.mappings["]"] = { ':lua require("harpoon.mark").add_file()<CR>', "Add buffer to Harpoon" }
+
+lvim.builtin.which_key.mappings["1"] = { ':lua require("harpoon.ui").nav_file(1)<CR>', "Go to Harpoon's 1st file" }
+lvim.builtin.which_key.mappings["2"] = { ':lua require("harpoon.ui").nav_file(2)<CR>', "Go to Harpoon's 2st file" }
+lvim.builtin.which_key.mappings["3"] = { ':lua require("harpoon.ui").nav_file(3)<CR>', "Go to Harpoon's 3st file" }
+lvim.builtin.which_key.mappings["4"] = { ':lua require("harpoon.ui").nav_file(4)<CR>', "Go to Harpoon's 4st file" }
+lvim.builtin.which_key.mappings["5"] = { ':lua require("harpoon.ui").nav_file(5)<CR>', "Go to Harpoon's 5st file" }
+
+-- Telescope
+lvim.builtin.which_key.mappings["S"] = {
+  ':lua require("telescope").extensions.live_grep_args.live_grep_args()<CR>',
+  "Find file with rg arguments"
+}
+
+-- Nvimtree
+lvim.builtin.nvimtree.setup.renderer.highlight_opened_files = '3'
+lvim.builtin.nvimtree.setup.hijack_cursor = true
+lvim.builtin.nvimtree.setup.update_focused_file = {
+  enable      = true,
+  update_cwd  = true,
+  ignore_list = { 'node_modules' },
+}
+
+-- Mateiral theme
+require('material').setup({
+
+  contrast = {
+    sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+    floating_windows = true, -- Enable contrast for floating windows
+    line_numbers = true, -- Enable contrast background for line numbers
+    sign_column = true, -- Enable contrast background for the sign column
+    cursor_line = false, -- Enable darker background for the cursor line
+    non_current_windows = true, -- Enable darker background for non-current windows
+    popup_menu = true, -- Enable lighter background for the popup menu
+  },
+
+  italics = {
+    comments = true, -- Enable italic comments
+    keywords = true, -- Enable italic keywords
+    functions = true, -- Enable italic functions
+    strings = false, -- Enable italic strings
+    variables = false -- Enable italic variables
+  },
+
+  contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
+    "terminal", -- Darker terminal background
+    "packer", -- Darker packer background
+    "qf" -- Darker qf list background
+  },
+
+  high_visibility = {
+    oceanic = true,
+    lighter = true,
+    darker = true,
+    palenight = true,
+  },
+
+  disable = {
+    borders = false, -- Disable borders between verticaly split windows
+    background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
+    term_colors = false, -- Prevent the theme from setting terminal colors
+    eob_lines = false -- Hide the end-of-buffer lines
+  },
+
+  lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+
+  async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+  custom_highlights = {} -- Overwrite highlights with your own
 })
 
+-- Telescope
+local lga_actions = require("telescope-live-grep-args.actions")
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+}
+lvim.builtin.telescope.extensions = {
+  live_grep_args = {
+    auto_quoting = true, -- enable/disable auto-quoting
+    mappings = {
+      i = {
+        ["<C-k>"] = lga_actions.quote_prompt(),
+        ["<C-f>"] = lga_actions.quote_prompt({ postfix = ' --iglob ' }),
+        ["<C-l>"] = lga_actions.quote_prompt({ postfix = ' -t' }),
+      }
+    }
+  }
+}
+
+-- which-key timeout
+vim.o.timeoutlen = 500
